9f59346531a31bb8b6e83e8571a0b25f5efc7539
Shlomi <shlomivaknin@gmail.com>
2017-11-07 21:25:35 -0800

    Setting up clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject matcher "0.1.0-SNAPSHOT"
+(defproject vadali/matcher "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.memoize "0.5.8"]
                  [com.rpl/specter "1.0.4"]
