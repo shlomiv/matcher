9b8ee2266a6f301e7a43cdddfa362f32d3e9e35b
Shlomi <shlomivaknin@gmail.com>
2017-11-07 21:52:42 -0800

    Added project url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject vadali/matcher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Matching and extracting data from deeply nested structures"
+  :url "https://github.com/vadali/matcher"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
